67a4950aa9a49d6749050210170173051489e380
Ivan Knysh <mr.rauber.ik@gmail.com>
2023-10-14 19:41:19 +0300

    Added scroll button

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -15,3 +15,37 @@ document.querySelector(".popup .close").addEventListener("click", () => {
 	document.querySelector(".popup").classList.remove("active");
 	document.querySelector(".overlay").classList.remove("active");
 });
+
+// Tabs
+const removeAllActiveClassesLi = () => {
+	document.querySelectorAll(".info .tab__nav .nav__item").forEach((item) => {
+		item.classList.remove("active");
+	});
+};
+
+document.querySelectorAll(".info .tab__nav .nav__item").forEach((item) => {
+	item.addEventListener("click", (e) => {
+		e.preventDefault();
+
+		const href = item.closest("li").querySelector("a").hash.substring(1);
+
+		removeAllActiveClassesLi();
+		item.classList.add("active");
+
+		item
+			.closest(".tab")
+			.querySelectorAll(".tab__content .tab__item")
+			.forEach((tabItem) => {
+				tabItem.classList.remove("active");
+
+				if (href === tabItem.getAttribute("id")) {
+					tabItem.classList.add("active");
+				}
+			});
+	});
+});
+
+// scroll
+document.querySelector("button.content__btn").addEventListener("click", () => {
+	document.querySelector(".info").scrollIntoView({ behavior: "smooth" });
+});
